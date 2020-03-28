b7957d434194549f9657ae8ee437d22b25c56315
Teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2020-03-28 21:07:31 +0700

    menambahkan project dengan commit pertama

---
+++ app/src/main/java/com/studiomasteguh/yourfirstinteractiveui/MainActivity.java
@@ -0,0 +1,55 @@
+package com.studiomasteguh.yourfirstinteractiveui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2,btn3;
+    TextView result;
+    private int resultshow = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn_count);
+        btn2 = findViewById(R.id.btn_toast);
+        btn3 = findViewById(R.id.btn_subtract);
+        result = findViewById(R.id.textView);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Ini adalah Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ++resultshow;
+                if (result != null)
+                result.setText(Integer.toString(resultshow));
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                --resultshow;
+                if (result != null)
+                    result.setText(Integer.toString(resultshow));
+            }
+        });
+    }
+
+}
+
